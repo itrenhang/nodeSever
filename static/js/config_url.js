222560eae56f52c27516393c17eaa4862dca7a5d
renhang <itrenhang@163.com>
2018-12-26 10:32:22 +0800

    edit

---
+++ static/js/config_url.js
@@ -0,0 +1,6 @@
+var urlList = {
+    indexBannerListUrl:'http://site.nobook.cc/api/slideShow',
+    indexNewsListUrl:'http://site.nobook.cc/api/news',
+    indexcasesListUrl:'http://site.nobook.cc/api/cases',
+    indexNewsDetailListUrl:'http://site.nobook.cc/api/newsDetail',
+}
